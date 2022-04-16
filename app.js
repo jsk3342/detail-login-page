2450a6646e5359242f0284cc227c92bfc73788b9
jsk3342 <kss2460@naver.com>
2022-04-16 21:07:28 +0900

    모달창 이벤트 구현

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ fullCheck.addEventListener("click", function () {
   emptyCheck.classList.remove("hidden");
   fullCheck.classList.add("hidden");
 });
+
+let loginBtu = document.querySelector(".login-btu");
+let loginContainer = document.querySelector(".login-container");
+let modalContainer = document.querySelector(".modal-container");
+let blackBackground = document.querySelector(".black-background");
+let closeIcon = document.querySelector(".modal-close-icon");
+
+loginBtu.addEventListener("click", function () {
+  loginContainer.classList.add("hidden");
+  modalContainer.classList.remove("hidden");
+  modalContainer.classList.add("modal-container-show");
+  blackBackground.classList.add("show-modal");
+});
+
+window.addEventListener("click", e => {
+  e.target === blackBackground
+    ? blackBackground.classList.remove("show-modal")
+    : false;
+  loginContainer.classList.remove("hidden");
+});
+
+closeIcon.addEventListener("click", function () {
+  blackBackground.classList.remove("show-modal");
+  loginContainer.classList.remove("hidden");
+});
