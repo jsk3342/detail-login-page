08cb190a520ad5d5e729cc219a95d2cd3b428ee2
jsk3342 <kss2460@naver.com>
2022-04-16 20:11:14 +0900

    로그인 상태 유지 기능 구현

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-document.querySelector
+let emptyCheck = document.getElementById("empty_check");
+let fullCheck = document.getElementById("full_check");
+
+emptyCheck.addEventListener("click", function () {
+  emptyCheck.classList.add("hidden");
+  fullCheck.classList.remove("hidden");
+});
+
+fullCheck.addEventListener("click", function () {
+  emptyCheck.classList.remove("hidden");
+  fullCheck.classList.add("hidden");
+});
